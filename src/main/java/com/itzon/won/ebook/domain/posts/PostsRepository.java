9b6d8d846e7ed3bac1dda0a396d47ce9e52cdd3a
1seok <jws7077@gmail.com>
2020-02-07 14:37:47 +0900

    lombok 적용 및 테스트

---
+++ src/main/java/com/itzon/won/ebook/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.itzon.won.ebook.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
---
+++ src/test/java/com/itzon/won/ebook/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.itzon.won.ebook.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("jws7077@gmail.com").build());
+    }
+}
