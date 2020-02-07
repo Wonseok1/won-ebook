d856a249b23fd48bbca32230be183cf6dcdbf479
1seok <jws7077@gmail.com>
2020-02-07 16:30:33 +0900

    JPA Auditing

--- src/test/java/com/itzon/won/ebook/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/itzon/won/ebook/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -23,7 +28,24 @@ public class PostsRepositoryTest {
     public void 게시글저장불러오기() {
         String title = "테스트 게시글";
         String content = "테스트 본문";
-
         postsRepository.save(Posts.builder().title(title).content(content).author("jws7077@gmail.com").build());
+    }
+
+    @Test
+    public void BaseTimeEntity등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2020, 2, 7, 16, 29, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>> createdDate = " + posts.getCreatedDate() + ", modifiedDate =" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
