03adc64a180d8b297f037f2eaa671382fd75455f
1seok <jws7077@gmail.com>
2020-02-07 18:00:43 +0900

    게시글 삭제

--- src/main/java/com/itzon/won/ebook/web/PostApiController.java
+++ src/main/java/com/itzon/won/ebook/web/PostApiController.java
@@ -29,4 +29,9 @@ public class PostApiController {
     }
 
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
